1de22c6b0b637a0f4133040bb9c8149fcafecba1
Andreas Klapperich <andreas@klapperich.de>
2019-03-09 18:18:32 +0100

    Add files via upload

---
+++ VisualStudioMac/project1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyAttributeTest
+{
+
+
+
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class BuildMachineNameAttribute : Attribute
+    {
+        public BuildMachineNameAttribute(string value)
+        {
+            MachineName = value;
+        }
+
+        public string MachineName { get; }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var entryAssembly = Assembly.GetEntryAssembly();
+            var property = entryAssembly?
+              .GetCustomAttribute<BuildMachineNameAttribute>()?
+              .MachineName ?? "";
+            Console.WriteLine($"BuildMachineName {property}");
+            Console.ReadLine();
+        }
+    }
+}
